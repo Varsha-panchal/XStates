60fd6106695e10db10642e0eb247a72f86fc1130
sangmesh panchal <you@example.com>
2024-02-04 00:11:13 +0530

    start

---
+++ xstates/src/App.js
@@ -0,0 +1,15 @@
+
+
+import React from 'react';
+import LocationSelector from './LocationSelector';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Location Selector App</h1>
+      <LocationSelector />
+    </div>
+  );
+};
+
+export default App;
